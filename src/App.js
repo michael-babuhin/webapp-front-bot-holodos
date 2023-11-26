bdd5efe3e4bf06ee08618b012a5398ec7ac0a87b
Michael-Babuhin <graf02030405@yandex.ru>
2023-11-26 23:00:20 +0700

    update react-app components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
-import {useEffect} from "react";
-const tg = window.Telegram.WebApp;
 function App() {
 
-    useEffect(() => {
-        tg.ready()
-    }, [])
-
-    const onClose = () => {
-        tg.close()
-    }
   return (
     <div className="App">
         <h2>Форма ввода информации для холодильника</h2>
